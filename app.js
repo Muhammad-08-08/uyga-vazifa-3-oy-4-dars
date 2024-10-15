3b7e84f175be58e3823b09e6e3183c9eddd363e6
muhammad <muhammadnuraliyev119@gmail.com>
2024-10-15 15:28:59 +0500

    muhammad

---
+++ app.js
@@ -0,0 +1,132 @@
+// 1-misol
+
+let sm = prompt("son kiriting smda");
+sm = parseFloat(sm);
+let m = parseInt(sm / 100);
+alert(`siz kiritgan son ${m} metr`);
+
+// 1-misoltugadi
+
+// 2-misol
+
+// let kg = prompt("kg kiriting");
+// kg = parseFloat(kg);
+// let t = parseInt(kg / 1000);
+// alert(`siz kiritgan son ${t} tonna`);
+
+// 2-misoltugadi
+
+// 3-misol
+
+// let kb = prompt("bayt kiriting");
+// kb = parseFloat(kb);
+// let b = parseInt(kb / 1024);
+// alert(`siz kiritgan son ${b} kilobayt`);
+
+// 3-misoltugadi
+
+// 4-misol
+
+// let a =+prompt("A-sonini kirgizing");
+// let b =+prompt("B-sonini kirgizing");
+
+// let c=a/b;
+
+// alert(c);
+
+// 4-misoltugadi
+
+// 5-misol
+
+// let a =+prompt("A-sonini kirgizing");
+// let b =+prompt("B-sonini kirgizing");
+// let c = a%b;
+// alert(c);
+
+// 5-misoltugadi
+
+// 6-misol
+
+// let son = +prompt("ikki xonali Son kiriting");
+
+// O'nliklar xonasini topish va chiqarish
+// let onliklar = Math.floor(son / 10);
+// alert("O'nliklar xonasi: " + onliklar);
+
+// // Birliklar xonasini topish va chiqarish
+// let birliklar = son % 10;
+// alert("Birliklar xonasi: " + birliklar);
+
+// 6-misoltugadi
+
+// 7-misol
+
+// let son = +prompt("Son kiriting");
+// // O'nliklar va birliklar xonasini topish
+// let onliklar = Math.floor(son / 10);
+// let birliklar = son % 10;
+
+// // Raqamlar yig'indisini hisoblash
+// let yigindi = onliklar + birliklar;
+// alert(yigindi);
+
+// 7-misoltugadi
+
+// 8-misol
+
+// let son = +prompt("ikki xonali Son kiriting");
+// let a = parseInt(son / 10);
+// let b = son % 10;
+// alert(`${b}${a}`);
+
+// 8-misoltugadi
+
+// 9-misol
+
+// let son = +prompt("uch xonali Son kiriting");
+// let a = parseInt(son / 100);
+// alert(a);
+
+// 9-misoltugadi
+
+// 10-misol
+
+// let son = +prompt("Uch xonali Son kiriting");
+// let birliklar = son % 10;
+// alert("Birliklar xonasi: " + birliklar);
+
+// // O'nliklar xonasini topish
+// let onliklar = Math.floor((son % 100) / 10);
+// alert("O'nliklar xonasi: " + onliklar);
+
+// 10-misol tugadi
+
+// 11-misol
+
+// let a = +prompt("A sonini kiriting");
+// let b = +prompt("B sonini kiriting");
+// let c = +prompt("C sonini kiriting");
+// let son = (a + b + c);
+// alert(son);
+
+// 11-misol tugadi
+
+// 12-misol
+
+// let son = +prompt("uch xonali Son kiriting");
+// let a = Math.floor(son / 100);
+// let b = Math.floor((son % 100) / 10);
+// let c = son % 10;
+// alert(`${c}${b}${a}`);
+
+// 12-misol tugadi
+
+// 13-misol
+
+// let son = +prompt("Uch xonali Son kiriting");
+// let a = Math.floor(son / 100);  
+// let b = Math.floor((son % 100) / 10);
+// let c = son % 10;
+// alert(`${b}${c}${a}`);
+
+// 13-misol tugadi
